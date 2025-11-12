437ff3c9026965ff078f66ea25381ae284c7f910
Kyu0 <efftar@naver.com>
2025-11-12 15:15:54 +0900

    Initial commit

---
+++ src/main/java/com/kyu0/churchmate/ChurchmateApplication.java
@@ -0,0 +1,13 @@
+package com.kyu0.churchmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChurchmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChurchmateApplication.class, args);
+	}
+
+}
